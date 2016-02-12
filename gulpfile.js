4871042a82242d41c3a4ed5398299ea500a999ce
Jon Eubank <joneubank@gmail.com>
2016-02-12 00:24:44 -0500

    gulpfile and dependencies prepared

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp            = require('gulp');
+var sass            = require('gulp-ruby-sass');
+var del             = require('del');
+var autoprefixer    = require('gulp-autoprefixer');
+var cssnano         = require('gulp-cssnano');
+var rename          = require('gulp-rename');
+var jshint          = require('gulp-jshint');
+var uglify          = require('gulp-uglify');
+var concat          = require('gulp-concat');
+var babel           = require('gulp-babel');
+
+
+gulp.task('clean', function() {
+    return del(['dist/css', 'dist/js']);
+});
+
+gulp.task('styles', function() {
+    return sass('./src/styles/main.scss')
+        .on('error', sass.logError)
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('./src/scripts/**/*.js')
+        .pipe(jshint({esversion: 6}))
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(babel())
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('html', function() {
+    return gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('watch', function(){
+    gulp.watch('./src/styles/*.scss', ['styles']);
+});
